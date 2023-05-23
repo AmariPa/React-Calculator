31754786bdafc4d307d7f87a44f3bb69b4ad6a15
Amari Pascoe <elliotpascoe04@gmail.com>
2023-05-23 11:30:05 -0400

    updated most buttons, currently encountering error with 'current'

--- src/App.js
+++ src/App.js
@@ -1,29 +1,51 @@
-import './App.css'
+import { useReducer } from 'react';
+import './App.css';
+import DigitButton from './Digit';
+
+export const ACTIONS = {
+  ADD_DIGIT: 'add-digit',
+  CHOOSE_OPERATION: 'choose-operation',
+  CLEAR: 'clear',
+  DELETE_DIGIT: 'delete-digit',
+  EVALUATE: 'evaluate',
+}
+
+function reducer(state, { type, payload }) {
+  switch(type) {
+    case ACTIONS.ADD_DIGIT: 
+      return {
+        ...state,
+        current: `${current || ''}${payload.digit}`
+      }
+  }
+}
 
 function App() {
+  const [{ current, previous, operation}, dispatch] = useReducer(reducer, {})
+
   return (
     <div className="calc">
       <div className='output'>
-        <div className='previous'>123, 234</div>
-        <div className='current'>123, 234</div>
+        <div className='previous'>{previous} {operation}</div>
+        <div className='current'>{current}</div>
       </div>
       <button className='big-button'>AC</button>
       <button>DEL</button>
-      <button>÷</button>
-      <button>1</button>
-      <button>2</button>
-      <button>3</button>
-      <button>×</button>
-      <button>4</button>
-      <button>5</button>
-      <button>6</button>
-      <button>+</button>
-      <button>7</button>
-      <button>8</button>
-      <button>9</button>
-      <button>−</button>
-      <button>.</button>
-      <button>0</button>
+      <DigitButton digit='÷' dispatch={dispatch} />
+      <DigitButton digit='1' dispatch={dispatch} />
+      <DigitButton digit='2' dispatch={dispatch} />
+      <DigitButton digit='3' dispatch={dispatch} />
+      <DigitButton digit='×' dispatch={dispatch} />
+      <DigitButton digit='4' dispatch={dispatch} />
+      <DigitButton digit='5' dispatch={dispatch} />
+      <DigitButton digit='6' dispatch={dispatch} />
+      <DigitButton digit='+' dispatch={dispatch} />
+      <DigitButton digit='7' dispatch={dispatch} />
+      <DigitButton digit='8' dispatch={dispatch} />
+      <DigitButton digit='9' dispatch={dispatch} />
+      <DigitButton digit='-' dispatch={dispatch} />
+      <DigitButton digit='.' dispatch={dispatch} />
+      <DigitButton digit='0' dispatch={dispatch} />
       <button className='big-button'>=</button>
     </div>
   );
